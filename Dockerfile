21c0db018a9097b4b6a26dcb3487af9185851f60
larry <ttttmmm3264@gmail.com>
2022-01-04 16:25:57 +0300

    switch pip install to pipenv

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM python:3.8-slim
 WORKDIR /app/
-ADD requirements.txt /app/
-RUN pip3 install --upgrade pip
-RUN pip3 install -r requirements.txt
 ADD . /app/
+RUN pip3 install --upgrade pip
+RUN pip3 install pipenv
+RUN pipenv install --system --ignore-pipfile
 RUN cd /app/
 CMD ["python3", "main.py"]
 
--- db/base.py
+++ db/base.py
@@ -1,5 +1,5 @@
 from databases import Database
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 from core.config import DATABASE_URL
@@ -8,5 +8,4 @@ from core.config import DATABASE_URL
 Base = declarative_base()
 database = Database(DATABASE_URL)
 engine = create_engine(DATABASE_URL)
-metadata = MetaData()
 
